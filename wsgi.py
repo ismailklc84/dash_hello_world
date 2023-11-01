6ad253e67dd6fcc5b535d0c10a5ca867e1897cdc
ismailklc84 <31347117+ismailklc84@users.noreply.github.com>
2023-11-01 21:40:39 +0300

    ok

--- wsgi.py
+++ wsgi.py
@@ -6,16 +6,26 @@ import os
 
 app = Dash()
 application = app.server
-app.layout = html.Div([
-    html.Div(children='Hello World')
-])
 
 redis_instance = redis.StrictRedis(host='redis', port=6379, password='redis-password')
 REDIS_HASH_NAME = os.environ.get("DASH_APP_NAME", "app-data")
 REDIS_KEYS = {"DATASET": "DATASET", "DATE_UPDATED": "DATE_UPDATED"}
+
 redis_instance.hset(
                 REDIS_HASH_NAME, REDIS_KEYS["DATE_UPDATED"], str(datetime.datetime.now())
     )
+
+data_last_updated = redis_instance.hget(
+        REDIS_HASH_NAME, REDIS_KEYS["DATE_UPDATED"]
+    ).decode("utf-8")
+
+app.layout = html.Div([
+    html.Div(children='Hello World'),
+    html.Div(children=data_last_updated)
+])
+
+
+
 
 if __name__ == '__main__':
     app.run_server(port=8080)
