2ecb5ffd4b9f536f8299740379b10b77a0941230
ismailklc84 <31347117+ismailklc84@users.noreply.github.com>
2023-11-01 21:50:25 +0300

    ok

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
-
+timeout = int(os.environ.get('TIMEOUT',600))
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
